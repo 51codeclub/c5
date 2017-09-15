54ed8c539dcded4e2a22e8f760ddaa501b0c709c
baosongshan <baosongshan2006@163.com>
2017-09-15 16:55:30 +0800

    add tool ut github

---
+++ ut/system_use/test.c
@@ -0,0 +1,13 @@
+/**********************************************************
+*   >File Name: test.c *    > Author: 51CC_baosongshan
+*    > Mail: baosongshan2006@163.com 
+*    > Created Time: 2017年09月15日 星期五 16时31分15秒
+**********************************************************/
+
+#include<stdio.h>
+int main()
+{
+    printf("Hello Linux.\n");
+    return 0;
+}
+
