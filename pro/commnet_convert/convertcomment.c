418b3234a055d22a1e5c4e1e7a3603fb8ba8f862
baosongshan <baosongshan2006@163.com>
2017-09-23 10:14:43 +0800

    add convertcomment in pro

---
+++ pro/commnet_convert/convertcomment.c
@@ -0,0 +1,23 @@
+
+/**********************************************************
+*    > File Name: convertcomment.c
+*    > Author: 51CC_baosongshan
+*    > Mail: baosongshan2006@163.com 
+*    > Created Time: 2017年09月23日 星期六 09时55分30秒
+**********************************************************/
+
+#include "convertcomment.h"
+
+int convertcomment(FILE *inputfile, FILE *outputfile)
+{
+    if(inputfile==NULL || outputfile==NULL)
+    {
+        printf("invalid arugment inputfile or outputfile.\n");
+        return -1;
+    }
+
+    /////////////////////////////////////////////////////////
+    //
+
+    return 0;
+}
