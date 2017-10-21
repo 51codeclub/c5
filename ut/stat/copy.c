b6cbfaa21ed602c63bd4330de8391e1a3ee0081a
angeltears <1114546349@qq.com>
2017-10-21 11:45:40 +0800

    delete

--- ut/stat/copy.c
+++ ut/stat/copy.c
@@ -1,11 +1,4 @@
 
-/**********************************************************
- *    > File Name: copy.c
- *    > Author: 51CC_baosongshan
- *    > Mail: baosongshan2006@163.com 
- *    > Created Time: 2017年10月13日 星期五 14时59分13秒
- **********************************************************/
-
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/stat.h>
